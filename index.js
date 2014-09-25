d28fb680092cd7dbda2060f90ecf8af5f0dc3d8a
Kevin Reilly <kevin.reilly@touchfactor.com>
2014-09-25 07:49:15 -0700

    Basic server setup

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express'),
+	  app = express(),
+	  server = require('http').Server(app),
+	  io = require('socket.io')(server);
+
+io.sockets.on('connection', function (socket) {
+  console.log('Received Socket.io connection:', socket.id);
+});
+
+server.listen(3000, function() {
+	console.log('Listening on port %d', server.address().port);
+});
